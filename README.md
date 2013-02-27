ca4260dd83f7487be18a4f5d4faa9001e684d085
Joseph Moore <josephrmoore@gmail.com>
2013-02-27 17:20:25 -0500

    friend scoring and acceptance in 1st choice

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ The current state of the algorithm is:
 
 1. For each thesis section, if the number of first choices is less than or equal to the number of spots, accept those students.
 2. If a thesis section has more interest than spots, the teacher's preferences are collected between all "interested students". Those who indicated that thesis as either a 1st, 2nd, or 3rd choice.
+3. If a student whose first choice is a contested section has been selected by the professor, that student is accepted.
+4. Students are scored by their and their friends' first choices.
+5. Students with the highest scores are accepted into contested sections until there are no spaces left.
+***** Soon *****
+6. Repeat steps 1-5 for second and third choices.
+7. Handle remaining students* 
+
+*still under thought construction
--- wow.js
+++ wow.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($){
 
+	var students_left = [];
 	var theses = generateTheses(7);
 	var students = generateStudents(80);
 	// console.log(students);
@@ -37,44 +38,169 @@ jQuery(document).ready(function($){
 		theses[i].selections.not_chosen = not_chosen;
 		theses[i].selections.chosen = first+second+third;
 	}
+
+	for(i=0;i<students.length;i++){
+		students_left.push(students[i].id);
+		for(k=0;k<students[i].preferences.friend_id.length;k++){
+			if(students[students[i].preferences.friend_id[k]].preferences.thesis_id[0] == students[i].preferences.thesis_id[0]){
+				students[i].score++;
+			}
+		}
+	}
+
 	var smallest = students.length;
 
 	for(i=0;i<theses.length;i++){
-		var students_left = [];
+		// There are fewer total 1st choices than spots available in the section
 		if(theses[i].selections.first <= theses[i].spaces.total){
 			for(j=0;j<students.length;j++){
+				// This student's first choice is this section
 				if(students[j].preferences.thesis_id[0] == i){
+					// add student
 					students[j].enrolled_thesis = i;
 					theses[i].enrolled_students.push(j);
-				}
-				if(students[j].enrolled_thesis<0){
-					students_left.push(students[j].id);
+					// update who's left
+					for(k=0;k<students_left.length;k++){
+						if(students_left[k] == students[j].id){
+							students_left.splice(k,1);
+						}
+					}
 				}
 			}
 		}
 		
+		// Set students a professor prefers. The 5 is the number of students a teacher can put dibs on.
 		var avail = uniqueRandom(Math.floor(Math.random()*5), Math.floor(Math.random()*interested_students.length));
 		if(theses[i].enrolled_students.length == 0){
 			for(j=0;j<avail.length;j++){
-			// The 5 is the number of students a teacher can put dibs on
-				theses[i].preferred_students.push(interested_students[avail[j]]);				
+				theses[i].preferred_students.push(interested_students[avail[j]]);	
 			}
 		}
 		
+		// Add teacher preferred students
 		for(j=0;j<theses[i].preferred_students.length;j++){
+			//
 			if(students[theses[i].preferred_students[j]].preferences.thesis_id[0] == i){
+				// add student
 				students[theses[i].preferred_students[j]].enrolled_thesis = i;
-				theses[i].enrolled_students.push(j);
+				theses[i].enrolled_students.push(theses[i].preferred_students[j]);
+				// update who's left
+				for(k=0;k<students_left.length;k++){
+					if(students_left[k] == students[theses[i].preferred_students[j]].id){
+						students_left.splice(k,1);
+					}
+				}
 			}
 		}
-		
-		$('.theses tbody').append('<tr><td>'+theses[i].id+'</td><td>'+theses[i].teacher+'</td><td>'+theses[i].selections.first+'</td><td>'+theses[i].selections.second+'</td><td>'+theses[i].selections.third+'</td><td>'+theses[i].selections.chosen+'</td><td>'+theses[i].selections.not_chosen+'</td><td>'+theses[i].preferred_students+'</td><td>'+theses[i].enrolled_students+'</td></tr>');
 	}
 
-	for(i=0;i<students.length;i++){
-		$('.students tbody').append('<tr><td>'+ students[i].id +'</td><td>'+ students[i].preferences.thesis_id[0] +' <em>'+theses[students[i].preferences.thesis_id[0]].teacher+'</em></td><td>'+ students[i].preferences.thesis_id[1] +' <em>'+theses[students[i].preferences.thesis_id[1]].teacher+'</em></td><td>'+ students[i].preferences.thesis_id[2] +' <em>'+theses[students[i].preferences.thesis_id[2]].teacher+'</em></td><td>'+ students[i].preferences.friend_id +'</td><td>'+students[i].enrolled_thesis+'</td></tr>');
+	var highest_score = 0;
+	var winner = [];
+	
+	for(j=0;j<students_left.length;j++){
+		if(students[students_left[j]].score > highest_score){
+			highest_score = students[students_left[j]].score;
+		}
+	}
+	for(j=highest_score;j>=0;j--){
+		winner[j] = [];
+		for(k=0;k<students_left.length;k++){
+			if(students[students_left[k]].score == j){
+				winner[j].push(students[students_left[k]]);
+			}
+		}
+	}
+	console.log(winner);
+
+	for(i=winner.length-1;i>=0;i--){
+		// console.log("i " + i);
+		for(j=0;j<winner[i].length;j++){
+			// console.log("j " + j);
+			var friend_pool = [];
+			for(k=0;k<winner[i][j].preferences.friend_id.length;k++){
+				// console.log("k " + k);
+				if(students[winner[i][j].preferences.friend_id[k]].preferences.thesis_id[0] == winner[i][j].preferences.thesis_id[0] && students[winner[i][j].preferences.friend_id[k]].enrolled_thesis<0){
+					// console.log(winner[i][j]);
+					friend_pool.push(students[winner[i][j].preferences.friend_id[k]]);
+				}
+			}
+			if(friend_pool.length<=theses[winner[i][j].preferences.thesis_id[0]].spaces.available-theses[winner[i][j].preferences.thesis_id[0]].enrolled_students.length){
+				for(k=0;k<friend_pool.length;k++){
+					// add student
+					friend_pool[k].enrolled_thesis = i;
+					theses[winner[i][j].preferences.thesis_id[0]].enrolled_students.push(friend_pool[k].id);
+					// update who's left
+					for(l=0;l<students_left.length;l++){
+						if(students_left[l].id == friend_pool[k].id){
+							students_left.splice(l,1);
+						}
+					}
+				}
+			}
+			console.log(friend_pool);
+		}
 	}
 	
+	for(i=0;i<theses.length;i++){
+		// if(theses[i].enrolled_students.length<theses[i].spaces.total){
+		// 	for(j=highest_score;j>=0;j--){
+		// 		for(k=0;k<winner[j].length;k++){
+		// 			var friend_pool = [];
+		// 			for(l=0;l<winner[j][k].preferences.friend_id.length;l++){
+		// 				if(students[winner[j][k].preferences.friend_id[l]].preferences.thesis_id[0] == winner[j][k].preferences.thesis_id[0]){
+		// 					friend_pool.push(winner[j][k].preferences.friend_id[l]);
+		// 				}
+		// 			}
+		// 		}
+		// 	}
+		// 	for(l=0;l<friend_pool.length;l++){
+		// 		console.log(students[friend_pool[l]]);
+		// 		// // add student
+		// 		// students[friend_pool[l]].enrolled_thesis = i;
+		// 		// theses[i].enrolled_students.push(friend_pool[l]);
+		// 		// // update who's left
+		// 		// for(m=0;m<students_left.length;m++){
+		// 		// 	if(students_left[m] == students[friend_pool[l]].id){
+		// 		// 		students_left.splice(m,1);
+		// 		// 	}
+		// 		// }
+		// 	}
+		// }
+		
+		// for(j=highest_score;j>=0;j--){
+		// 	for(k=0;k<winner[j].length;k++){
+		// 		var friend_pool = [];
+		// 		for(l=0;l<winner[j][k].preferences.friend_id.length;l++){
+		// 			if(students[winner[j][k].preferences.friend_id[l]].preferences.thesis_id[0] == winner[j][k].preferences.thesis_id[0]){
+		// 				friend_pool.push(winner[j][k].preferences.friend_id[l]);
+		// 			}
+		// 		}
+		// 	
+		// 	}
+		// 	if(friend_pool.length <= theses[i].spaces.total){
+		// 		// console.log(friend_pool);
+		// 		// console.log(theses[i].spaces.total);
+		// 		for(l=0;l<friend_pool.length;l++){
+		// 			console.log(students[friend_pool[l]]);
+		// 			// // add student
+		// 			// students[friend_pool[l]].enrolled_thesis = i;
+		// 			// theses[i].enrolled_students.push(friend_pool[l]);
+		// 			// // update who's left
+		// 			// for(m=0;m<students_left.length;m++){
+		// 			// 	if(students_left[m] == students[friend_pool[l]].id){
+		// 			// 		students_left.splice(m,1);
+		// 			// 	}
+		// 			// }
+		// 		}
+		// 	}
+		// }
+		// console.log(theses[i].preferred_students);
+		$('.theses tbody').append('<tr><td>'+theses[i].id+'</td><td>'+theses[i].teacher+'</td><td>'+theses[i].selections.first+'</td><td>'+theses[i].selections.second+'</td><td>'+theses[i].selections.third+'</td><td>'+theses[i].selections.chosen+'</td><td>'+theses[i].selections.not_chosen+'</td><td>'+theses[i].preferred_students+'</td><td>'+theses[i].enrolled_students+'</td></tr>');
+	}
+	
+	for(i=0;i<students.length;i++){
+		$('.students tbody').append('<tr><td>'+ students[i].id +'</td><td>'+ students[i].preferences.thesis_id[0] +' <em>'+theses[students[i].preferences.thesis_id[0]].teacher+'</em></td><td>'+ students[i].preferences.thesis_id[1] +' <em>'+theses[students[i].preferences.thesis_id[1]].teacher+'</em></td><td>'+ students[i].preferences.thesis_id[2] +' <em>'+theses[students[i].preferences.thesis_id[2]].teacher+'</em></td><td>'+ students[i].preferences.friend_id +'</td><td>'+students[i].enrolled_thesis+'</td><td>'+students[i].score+'</td></tr>');
+	}
 	
 	function generateStudents(total){
 		var students = [];
@@ -89,7 +215,8 @@ jQuery(document).ready(function($){
 					],
 					"friend_id": f
 				},
-				"enrolled_thesis" : -1
+				"enrolled_thesis" : -1,
+				"score" : 0
 			};
 		}
 		return students;
