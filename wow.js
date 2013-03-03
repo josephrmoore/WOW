d0617efa6efe6db50d3352818674b7dc8c977fbc
Joseph Moore <josephrmoore@gmail.com>
2013-03-03 16:00:52 -0500

    dataviz charts and some small fixes

--- wow.js
+++ wow.js
@@ -1,14 +1,20 @@
+var got_first = 0;
+var got_second = 0;
+var got_third = 0;
+var got_none = 0;
+var got_one = 0;
+var got_friends = 0;
+
 jQuery(document).ready(function($){
 
 	// Initialize global variables - generateThesis ***MUST*** always be before generateStudents
 	var students_left = [];
 	var theses = generateTheses(7);
 	var students = generateStudents(80);
-	var dataviz;
 	// This is the algorithm
 	sausage_factory();
 	// Get dataviz data
-	 dataviz = dataviz();
+	var dataviz = dataviz();
 	
 	// Print to screen
 	for(var i=0;i<theses.length;i++){
@@ -155,11 +161,11 @@ jQuery(document).ready(function($){
 		
 	// The remaining sections are contested
 
-	// Iterations (x3)
+	// Iterations (x3) 1st, 2nd, & 3rd choices for thesis
 		for(var n=0;n<3;n++){
 			// For each section...
 			for(var i=0;i<theses.length;i++){
-				// Find out which students selected that section as a 1st choice
+				// Find out which students selected that section as this iteration's choice (1st, 2nd, or 3rd)
 				var choosers = getChoice(i, n);
 				for(var j=0;j<theses[i].teacher_pref.length;j++){
 					for(var k=0;k<choosers.length;k++){
@@ -176,7 +182,7 @@ jQuery(document).ready(function($){
 					// If they are now enrolled in this section...
 					if(students[j].thesis == i){
 						for(var k=0;k<students[j].friends.length;k++){
-							// If any of their friends selected this section as a first choice and there's still room...
+							// If any of their friends selected this section as this iteration's choice (1st, 2nd, 3rd) and there's still room...
 							if(students[students[j].friends[k]].choices[n] == i && theses[i].enrolled.length<theses[i].total){
 								// The friend is enrolled in the section
 								addStudent(students[students[j].friends[k]], i);
@@ -210,6 +216,7 @@ jQuery(document).ready(function($){
 					}
 				}
 			}
+			// Repeat for 2nd & 3rd choices...
 		}
 	// If there are any students left...
 		// For each section...
@@ -254,12 +261,6 @@ jQuery(document).ready(function($){
 	}
 	
 	function dataviz(){
-		var got_first = 0;
-		var got_second = 0;
-		var got_third = 0;
-		var got_none = 0;
-		var got_one = 0;
-		var got_friends = 0;
 
 		for(var i=0;i<students.length;i++){
 			if(students[i].choices[0] == students[i].thesis){
@@ -300,6 +301,87 @@ jQuery(document).ready(function($){
 
 		$('.dataviz tbody').append('<tr><td>'+dataviz.got_first+' -> '+ (dataviz.got_first/students.length)*100 +'%</td><td>'+dataviz.got_second+' -> '+ (dataviz.got_second/students.length)*100+'%</td><td>'+dataviz.got_third+' -> '+ (dataviz.got_third/students.length)*100+'%</td><td>'+dataviz.got_one+' -> '+ (dataviz.got_one/students.length)*100+'%</td><td>'+dataviz.got_none+' -> '+ (dataviz.got_none/students.length)*100+'%</td><td>'+dataviz.got_friends+' -> '+ (dataviz.got_friends/students.length)*100 +'%</td></tr>');
 
+
 	}
 	
+	
 });
+
+// Load the Visualization API and the piechart package.
+  google.load('visualization', '1.0', {'packages':['corechart']});
+
+  // Set a callback to run when the Google Visualization API is loaded.
+  google.setOnLoadCallback(drawChart);
+
+  // Callback that creates and populates a data table,
+  // instantiates the pie chart, passes in the data and
+  // draws it.
+function drawChart(){
+	drawChart1();
+	drawChart2();
+	drawChart3();
+}
+  function drawChart1() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Choice');
+    data.addColumn('number', 'Students');
+    data.addRows([
+      ['1st Choice', got_first],
+      ['2nd Choice', got_second],
+      ['3rd Choice', got_third],
+      ['No choice', got_none],
+    ]);
+
+    // Set chart options
+    var options = {'title':'Students by choice',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div1'));
+    chart.draw(data, options);
+  }
+
+  function drawChart2() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Choice');
+    data.addColumn('number', 'Students');
+    data.addRows([
+      ['Got one of their choices', got_one],
+      ['Didn\'t get any of their choices', got_none],
+    ]);
+
+    // Set chart options
+    var options = {'title':'Got One vs. Got None',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
+    chart.draw(data, options);
+  }
+
+  function drawChart3() {
+
+    // Create the data table.
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Choice');
+    data.addColumn('number', 'Students');
+    data.addRows([
+      ['In a section with a friend', got_friends],
+      ['In a section with no friends', (80-got_friends)]
+    ]);
+
+    // Set chart options
+    var options = {'title':'Students by Friends',
+                   'width':400,
+                   'height':300};
+
+    // Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart_div3'));
+    chart.draw(data, options);
+  }
